288d0978c90621eaa5cd9670d5c7fc402c0e3dbb
gabrax <gabrax@Linux.myguest.virtualbox.org>
2024-03-25 23:46:57 +0100

    needs fixes

---
+++ Client/client.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define port 2137
+
+int main()
+{
+    int sock = 0; 
+    struct sockaddr_in serv_adrr;
+    std::string hello = "Hello from client\n";
+    char buffer[1024] = {0};
+    if((sock = socket(AF_INET, SOCK_STREAM,0)) < 0){
+        puts("\n Socket creation error \n");
+        return -1;
+    }
+
+    std::memset(&serv_adrr, '0', sizeof(serv_adrr));
+
+    serv_adrr.sin_family = AF_INET;
+    serv_adrr.sin_port = htons(port);
+
+    if(inet_pton(AF_INET,"127.0.0.1",&serv_adrr.sin_addr)<=0){
+        puts("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+    if(connect(sock,(struct sockaddr *)&serv_adrr, sizeof(serv_adrr)) < 0){
+        puts("\n Connection failed \n");
+        return -1;
+    }
+
+    send(sock,hello.c_str(),hello.size(),0);
+    puts("Hello message sent\n");
+    
+    #pragma GCC diagnostic ignored "-Wunused-variable"
+    long long valread = read(sock,buffer,1024);
+    std::cout << buffer << '\n';
+
+
+    return 0;
+}
---
+++ Server/host.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+
+#define port 2137
+
+int main()
+{
+
+    int server_socket, client_socket; 
+    
+
+    // Read HTML file
+    FILE *html;
+    html = fopen("index.html","r");
+
+    char response[1024];
+    fgets(response,1024,html);
+
+    char http_header[2048] = "HTTP/1.1 200 OK\r\n\n";
+    strcat(http_header, response);
+
+    if((server_socket = socket(AF_INET,SOCK_STREAM,0)) == 0){
+        perror("Socket error");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = INADDR_ANY;
+
+    //std::memset(address.sin_zero, '\0', sizeof address.sin_zero);
+
+    if(bind(server_socket, (struct sockaddr *) &address, sizeof(address)) < 0){
+        perror("Bind error");
+        exit(EXIT_FAILURE);
+    }
+    if(listen(server_socket, 5) < 0){
+        perror("Listen error");
+        exit(EXIT_FAILURE);
+    }
+
+    //int addrlen = sizeof(address);
+    while (1)
+    {
+        puts("\n--------- Waiting for connection ---------\n\n");
+        
+        if((client_socket = accept(server_socket,NULL,NULL)) < 0){
+            perror("Accepting failed");
+            exit(EXIT_FAILURE);
+        }
+
+
+        if(send(client_socket,http_header,sizeof(http_header), 0) < 0){
+            perror("Send failed");
+        }
+
+        puts("------------HTML Sent------------\n");
+        close(client_socket);
+    }
+
+    return 0;
+}
