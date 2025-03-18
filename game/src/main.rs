707f87a659b56988a0d97b5298fa16fbb6266f3a
Gabrax <gab-oz@wp.pl>
2025-03-18 11:43:29 +0100

    strafing + rotating by mouse

--- game/src/main.rs
+++ game/src/main.rs
@@ -11,18 +11,18 @@ static MAPDATA: [u8; MAP_SIZE * MAP_SIZE] = [
     1, 0, 0, 0, 0, 0, 0, 1,
     1, 0, 2, 0, 4, 4, 0, 1,
     1, 0, 0, 0, 4, 0, 0, 1,
-    1, 0, 3, 0, 0, 0, 0, 1,
+    1, 0, 2, 0, 0, 0, 0, 1,
     1, 1, 1, 1, 1, 1, 1, 1,
 ];
 
 #[derive(Copy, Clone)]
-struct State {
+struct Player {
     pos: Vector2,
     dir: Vector2,
-    plane: Vector2,
+    projection: Vector2,
 }
 
-impl State {
+impl Player {
     fn new() -> Self {
         let dir = {
             let v = Vector2::new(-1.0, 0.1);
@@ -30,10 +30,10 @@ impl State {
             Vector2::new(v.x / len, v.y / len)
         };
         
-        State {
+        Player {
             pos: Vector2::new(2.0, 2.0),
             dir,
-            plane: Vector2::new(0.0, 0.66),
+            projection: Vector2::new(0.0, 0.66),
         }
     }
 
@@ -44,13 +44,13 @@ impl State {
         let new_dir_x = self.dir.x * cos_rot - self.dir.y * sin_rot;
         let new_dir_y = self.dir.x * sin_rot + self.dir.y * cos_rot;
         
-        let new_plane_x = self.plane.x * cos_rot - self.plane.y * sin_rot;
-        let new_plane_y = self.plane.x * sin_rot + self.plane.y * cos_rot;
+        let new_plane_x = self.projection.x * cos_rot - self.projection.y * sin_rot;
+        let new_plane_y = self.projection.x * sin_rot + self.projection.y * cos_rot;
 
         self.dir.x = new_dir_x;
         self.dir.y = new_dir_y;
-        self.plane.x = new_plane_x;
-        self.plane.y = new_plane_y;
+        self.projection.x = new_plane_x;
+        self.projection.y = new_plane_y;
     }
 }
 
@@ -60,16 +60,16 @@ fn verline(d: &mut RaylibDrawHandle, x: i32, y0: i32, y1: i32, color: Color) {
     }
 }
 
-fn render(d: &mut RaylibDrawHandle, state: &State) {
+fn render(d: &mut RaylibDrawHandle, player: &Player) {
     for x in 0..SCREEN_WIDTH {
         let xcam = 2.0 * (x as f32) / SCREEN_WIDTH as f32 - 1.0;
 
         let dir = Vector2::new(
-            state.dir.x + state.plane.x * xcam,
-            state.dir.y + state.plane.y * xcam,
+            player.dir.x + player.projection.x * xcam,
+            player.dir.y + player.projection.y * xcam,
         );
 
-        let pos = state.pos;
+        let pos = player.pos;
         let mut ipos = Vector2::new(pos.x.floor(), pos.y.floor());
 
         let deltadist = Vector2::new(
@@ -78,8 +78,16 @@ fn render(d: &mut RaylibDrawHandle, state: &State) {
         );
 
         let mut sidedist = Vector2::new(
-            if dir.x < 0.0 { (pos.x - ipos.x) * deltadist.x } else { (ipos.x + 1.0 - pos.x) * deltadist.x },
-            if dir.y < 0.0 { (pos.y - ipos.y) * deltadist.y } else { (ipos.y + 1.0 - pos.y) * deltadist.y },
+            if dir.x < 0.0 { 
+                (pos.x - ipos.x) * deltadist.x
+            } else { 
+                (ipos.x + 1.0 - pos.x) * deltadist.x 
+            },
+            if dir.y < 0.0 { 
+                (pos.y - ipos.y) * deltadist.y
+            } else { 
+                (ipos.y + 1.0 - pos.y) * deltadist.y 
+            },
         );
 
         let step = Vector2::new(dir.x.signum(), dir.y.signum());
@@ -110,10 +118,10 @@ fn render(d: &mut RaylibDrawHandle, state: &State) {
         let mut color = Color::BLACK;
 
         match hit.0 {
-            1 => color = Color::new(255, 0, 0, 255),
-            2 => color = Color::new(0, 255, 0, 255),
-            3 => color = Color::new(255, 0, 0, 255),
-            4 => color = Color::new(255, 0, 255, 255),
+            1 => color = Color::RED,
+            2 => color = Color::GREEN,
+            3 => color = Color::BLUE,
+            4 => color = Color::MAGENTA,
             _ => {}
         }
 
@@ -138,36 +146,40 @@ fn main() {
     let (mut rl, thread) = raylib::init()
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
         .title("Raycasting with Rust")
+        .vsync()
         .build();
-    rl.set_target_fps(60);
 
-    let mut state = State::new();
+    let mut player = Player::new();
 
+    rl.disable_cursor();
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
 
-        let rotspeed = 3.0 * 0.016;
+        let mouse_delta = d.get_mouse_delta();
+        let mouse_sensitivity = 0.003;
+        player.rotate(-mouse_delta.x * mouse_sensitivity); 
+
         let movespeed = 3.0 * 0.016;
-        
-        if d.is_key_down(KeyboardKey::KEY_LEFT) {
-            state.rotate(rotspeed);
+        if d.is_key_down(KeyboardKey::KEY_W) {
+            player.pos.x += player.dir.x * movespeed;
+            player.pos.y += player.dir.y * movespeed;
         }
-
-        if d.is_key_down(KeyboardKey::KEY_RIGHT) {
-            state.rotate(-rotspeed);
+        if d.is_key_down(KeyboardKey::KEY_S) {
+            player.pos.x -= player.dir.x * movespeed;
+            player.pos.y -= player.dir.y * movespeed;
         }
-
-        if d.is_key_down(KeyboardKey::KEY_UP) {
-            state.pos.x += state.dir.x * movespeed;
-            state.pos.y += state.dir.y * movespeed;
+        if d.is_key_down(KeyboardKey::KEY_A) { 
+            player.pos.x -= player.dir.y * movespeed;
+            player.pos.y += player.dir.x * movespeed;
         }
-
-        if d.is_key_down(KeyboardKey::KEY_DOWN) {
-            state.pos.x -= state.dir.x * movespeed;
-            state.pos.y -= state.dir.y * movespeed;
+        if d.is_key_down(KeyboardKey::KEY_D) { 
+            player.pos.x += player.dir.y * movespeed;
+            player.pos.y -= player.dir.x * movespeed;
         }
 
         d.clear_background(Color::WHITE);
-        render(&mut d, &state);
+        render(&mut d, &player);
+        let position_text = format!("Position: {}: {}", player.pos.x as i32, player.pos.y as i32);
+        d.draw_text(&position_text, 0, 0, 6, Color::RAYWHITE);
     }
 }
