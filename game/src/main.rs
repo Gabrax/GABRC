d82a97fb380267b999c21246bd3c99804adf1e1f
Gabrax <gab-oz@wp.pl>
2025-03-21 17:17:33 +0100

    moved back to CPU, pixels are rendered onto framebuffer

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,7 +1,5 @@
-use raylib::ffi::UnloadImage;
 use raylib::prelude::*;
 
-use std::fmt;
 use std::rc::Rc;
 use std::cell::RefCell;
 mod player;
@@ -14,136 +12,62 @@ use crate::gamemap::draw_board;
 mod raycaster;
 use raycaster::Raycaster;
 
-const SCREEN_WIDTH: i32 = 800;
-const SCREEN_HEIGHT: i32 = 600;
+const BUFFER_WIDTH: i32 = 384;
+const BUFFER_HEIGHT: i32 = 216;
 
 fn main() {
-
-    let _map: Vec<u8> = vec![
-        1, 1, 1, 1, 1, 1, 1, 1,
-        1, 0, 0, 0, 0, 0, 0, 1,
-        1, 0, 0, 0, 0, 3, 0, 1,
-        1, 0, 0, 0, 0, 0, 0, 1,
-        1, 0, 2, 0, 4, 4, 0, 1,
-        1, 0, 0, 0, 4, 0, 0, 1,
-        1, 0, 2, 0, 0, 0, 0, 1,
-        1, 1, 1, 1, 1, 1, 1, 1,
-    ];
-
     let (mut rl, thread) = raylib::init()
-        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
-        .title("GPU Raycast")
+        .size(1280, 720)
+        .title("Raycasting with Rust")
         .vsync()
         .build();
-    //rl.set_target_fps(60);
+
     rl.disable_cursor();
 
-    let mut _shader = rl.load_shader(&thread,None,Some("res/base.glsl"),).unwrap();
 
-    let _map_loc = _shader.get_shader_location("mapData");
-    let _player_pos_x_loc = _shader.get_shader_location("player_pos_x");
-    let _player_pos_y_loc = _shader.get_shader_location("player_pos_y");
-    let _player_dir_x_loc = _shader.get_shader_location("player_dir_x");
-    let _player_dir_y_loc = _shader.get_shader_location("player_dir_y");
-    let _player_proj_x_loc = _shader.get_shader_location("player_proj_x");
-    let _player_proj_y_loc = _shader.get_shader_location("player_proj_x");
+    let texture_files = [
+        "res/greystone.png",
+        "res/wood.png",
+        "res/mossy.png",
+        "res/purplestone.png",
+        "res/redbrick.png",
+        "res/colorstone.png",
+        "res/bluestone.png",
+        "res/eagle.png",
+        "res/barrel.png",
+        "res/pillar.png",
+        "res/greenlight.png",
+        "res/chomik.png",
+    ];
 
-    let _map_f32: Vec<f32> = _map.iter().map(|&v| v as f32).collect();
+    let textures: Vec<Rc<RefCell<Image>>> = texture_files
+        .iter()
+        .map(|&path| Rc::new(RefCell::new(Image::load_image(path).expect("Failed to load texture"))))
+        .collect();
 
-    let mut target = rl.load_render_texture(&thread, SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32).unwrap();
+    let game_map = Rc::new(RefCell::new(GameMap::load_from_file("res/level_1.txt")));
 
-    let mut _player = Player::new();
+    let player = Rc::new(RefCell::new(Player::new()));
+    let _framebuffer = rl
+    .load_render_texture(&thread, BUFFER_WIDTH as u32, BUFFER_HEIGHT as u32)
+    .unwrap();
 
-    while !rl.window_should_close()
-    {
+    let mut raycaster = Raycaster::new(
+        BUFFER_WIDTH,
+        BUFFER_HEIGHT,
+        _framebuffer,
+        Rc::clone(&player),
+        textures,
+        Rc::clone(&game_map),
+    );
+
+    while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
 
+        raycaster.render_all(&mut d);
+        player.borrow_mut().update(&mut d);
 
-        _player.update(&mut d);
-
-        //println!("{}", _player.dir.x);
-
-        _shader.set_shader_value_v(_map_loc, &_map_f32);
-        _shader.set_shader_value(_player_pos_x_loc, _player.pos.x);
-        _shader.set_shader_value(_player_pos_y_loc, _player.pos.y);
-        _shader.set_shader_value(_player_dir_x_loc, _player.dir.x);
-        _shader.set_shader_value(_player_dir_y_loc, _player.dir.x);
-        _shader.set_shader_value(_player_proj_x_loc, _player.projection.x);
-        _shader.set_shader_value(_player_proj_y_loc, _player.projection.x);
-
-
-        {
-            let mut d = d.begin_texture_mode(&thread, &mut target); // Enable drawing to texture
-
-            //d.clear_background(Color::RAYWHITE); 
-            {
-
-            }
-        }
-
-        {
-            let mut d = d.begin_shader_mode(&_shader);
-
-            d.draw_texture_rec(
-                target.texture(),
-                rrect(0, 0, target.texture.width, -target.texture.height),
-                rvec2(0, 0),
-                Color::WHITE,
-            );
-        }
-
-        d.draw_fps(700, 15);
+        draw_board(&mut d, &player.borrow(), &game_map.borrow());
+        d.draw_fps(15, 0);
     }
 }
-
-//fn main() {
-//    let (mut rl, thread) = raylib::init()
-//        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
-//        .title("Raycasting with Rust")
-//        .vsync()
-//        .build();
-//
-//    rl.disable_cursor();
-//
-//    let texture_files = [
-//        "res/greystone.png",
-//        "res/wood.png",
-//        "res/mossy.png",
-//        "res/purplestone.png",
-//        "res/redbrick.png",
-//        "res/colorstone.png",
-//        "res/bluestone.png",
-//        "res/eagle.png",
-//        "res/barrel.png",
-//        "res/pillar.png",
-//        "res/greenlight.png",
-//        "res/chomik.png",
-//    ];
-//
-//    let textures: Vec<Rc<RefCell<Image>>> = texture_files
-//        .iter()
-//        .map(|&path| Rc::new(RefCell::new(Image::load_image(path).expect("Failed to load texture"))))
-//        .collect();
-//
-//    let game_map = Rc::new(RefCell::new(GameMap::load_from_file("res/level_1.txt")));
-//
-//    let player = Rc::new(RefCell::new(Player::new()));
-//
-//    let raycaster = Raycaster::new(
-//        SCREEN_WIDTH,
-//        SCREEN_HEIGHT,
-//        Rc::clone(&player),
-//        textures,
-//        Rc::clone(&game_map),
-//    );
-//
-//    while !rl.window_should_close() {
-//        let mut d = rl.begin_drawing(&thread);
-//
-//        raycaster.render_all(&mut d);
-//        player.borrow_mut().update(&mut d);
-//
-//        draw_board(&mut d, &player.borrow(), &game_map.borrow());
-//        d.draw_fps(15, 0);
-//    }
-//}
--- game/src/raycaster.rs
+++ game/src/raycaster.rs
@@ -11,22 +11,24 @@ use crate::GameMap;
 pub struct Raycaster {
     pub screen_width: i32,
     pub screen_height: i32,
-    pub pixelbuffer: Vec<Vec<u32>>,
-    pub player: Rc<RefCell<Player>>,   // Allow mutation
-    pub textures: Vec<Rc<RefCell<Image>>>,  // Use Rc<RefCell<Image>> for shared and mutable ownership
-    pub _map: Rc<RefCell<GameMap>>,    // Shared ownership of game map
+    pub player: Rc<RefCell<Player>>,   
+    pub textures: Vec<Rc<RefCell<Image>>>,  
+    pub _map: Rc<RefCell<GameMap>>,    
+    pixelbuffer: Vec<u32>,
+    _framebuffer: RenderTexture2D,
 }
 
 impl Raycaster {
-    // Constructor
     pub fn new(
         screen_width: i32,
         screen_height: i32,
+        // NOTE THIS IS FRAMEBUFFER which allows to render low resoution to high window resolution
+        _framebuffer: RenderTexture2D,
         player: Rc<RefCell<Player>>,
         textures: Vec<Rc<RefCell<Image>>>,  // Use Rc<RefCell<Image>> for mutable access
         _map: Rc<RefCell<GameMap>>,
     ) -> Self {
-        let pixelbuffer = vec![vec![0; screen_width as usize]; screen_height as usize];
+        let pixelbuffer = vec![0; (screen_width * screen_height) as usize];
 
         Raycaster {
             screen_width,
@@ -35,15 +37,34 @@ impl Raycaster {
             player,
             textures,
             _map,
+            _framebuffer,
         }
     }
 
-    pub fn render_all(&self, d: &mut RaylibDrawHandle) {
-        self.render_floor_ceiling(d);
-        self.render_walls(d);
+    pub fn render_all(&mut self, d: &mut RaylibDrawHandle) {
+        self.render_floor_ceiling();
+        self.render_walls();
+
+        self._framebuffer
+            .texture_mut()
+            .update_texture(bytemuck::cast_slice(&self.pixelbuffer));
+        
+        d.draw_texture_pro(
+            self._framebuffer.texture(), // Source texture
+            rrect(0, 0, self.screen_width as f32, -self.screen_height as f32), // Source rectangle (flipped vertically)
+            rrect(0, 0, d.get_screen_width() as f32, d.get_screen_height() as f32), // Destination rectangle (stretched to window)
+            rvec2(0.0, 0.0), // Origin
+            0.0, // Rotation (0 means no rotation)
+            Color::WHITE, // Tint (WHITE means no color modification)
+        );
     }
 
-    pub fn render_floor_ceiling(&self, d: &mut RaylibDrawHandle) {
+    pub fn color_to_u32(color: Color) -> u32 {
+        // Swap red and blue channels for BGRA format
+        ((color.a as u32) << 24) | ((color.b as u32) << 16) | ((color.g as u32) << 8) | (color.r as u32)
+    }
+
+    pub fn render_floor_ceiling(&mut self) {
         let mut floor_texture = self.textures[3].borrow_mut();  // Borrow the image immutably
         let mut ceiling_texture = self.textures[6].borrow_mut();  // Borrow the image immutably
 
@@ -70,28 +91,29 @@ impl Raycaster {
                 let cell_x = floor_x as i32;
                 let cell_y = floor_y as i32;
 
-                let tex_x = ((floor_x - cell_x as f32) * floor_texture.width as f32) as i32 & (floor_texture.width as i32 - 1);
-                let tex_y = ((floor_y - cell_y as f32) * floor_texture.height as f32) as i32 & (floor_texture.height as i32 - 1);
+                let tex_x = ((floor_x - cell_x as f32) * floor_texture.width as f32) as i32 & (floor_texture.width - 1);
+                let tex_y = ((floor_y - cell_y as f32) * floor_texture.height as f32) as i32 & (floor_texture.height - 1);
 
                 floor_x += floor_step_x;
                 floor_y += floor_step_y;
 
                 let floor_color = floor_texture.get_color(tex_x, tex_y);
-                d.draw_pixel(x as i32, y as i32, floor_color);
+                // Store the floor pixel color in pixelbuffer
+                self.pixelbuffer[(y * self.screen_width + x) as usize] = Self::color_to_u32(floor_color);
 
                 let ceiling_color = ceiling_texture.get_color(tex_x, tex_y);
-                d.draw_pixel(x as i32, (self.screen_height - y - 1) as i32, ceiling_color);
+                // Store the ceiling pixel color in pixelbuffer (mirrored y-coordinate)
+                self.pixelbuffer[((self.screen_height - y - 1) * self.screen_width + x) as usize] = Self::color_to_u32(ceiling_color);
             }
         }
     }
 
 
-    pub fn render_walls(&self, d: &mut RaylibDrawHandle) {
+    pub fn render_walls(&mut self) {
         let player = self.player.borrow();
         let _map = self._map.borrow();
 
         for x in 0..self.screen_width {
-            // Calculate ray position and direction
             let xcam = 2.0 * (x as f32) / self.screen_width as f32 - 1.0;
             let dir = Vector2::new(
                 player.dir.x + player.projection.x * xcam,
@@ -101,13 +123,11 @@ impl Raycaster {
             let pos = player.pos;
             let mut ipos = Vector2::new(pos.x.floor(), pos.y.floor());
 
-            // Length of ray from current position to next x or y-side
             let deltadist = Vector2::new(
                 if dir.x.abs() < 1e-20 { 1e30 } else { 1.0 / dir.x.abs() },
                 if dir.y.abs() < 1e-20 { 1e30 } else { 1.0 / dir.y.abs() },
             );
 
-            // Length of ray from one x or y-side to next x or y-side
             let mut sidedist = Vector2::new(
                 if dir.x < 0.0 { (pos.x - ipos.x) * deltadist.x } else { (ipos.x + 1.0 - pos.x) * deltadist.x },
                 if dir.y < 0.0 { (pos.y - ipos.y) * deltadist.y } else { (ipos.y + 1.0 - pos.y) * deltadist.y },
@@ -116,7 +136,6 @@ impl Raycaster {
             let step = Vector2::new(dir.x.signum(), dir.y.signum());
             let mut hit = (0, 0);
 
-            // Perform DDA (Digital Differential Analysis)
             while hit.0 == 0 {
                 if sidedist.x < sidedist.y {
                     sidedist.x += deltadist.x;
@@ -138,26 +157,26 @@ impl Raycaster {
                 hit.0 = _map.map_data[map_y as usize * _map.size + map_x as usize] as i32;
             }
 
-            // Select the texture based on the wall hit type
-            let mut texture = match hit.0 {
-                1 => self.textures[0].borrow_mut(), 
-                2 => self.textures[4].borrow_mut(), 
-                3 => self.textures[11].borrow_mut(), 
-                4 => self.textures[2].borrow_mut(), 
-                _ => self.textures[0].borrow_mut(), 
+            // Select the correct texture based on wall type
+            let texture_index = match hit.0 {
+                1 => 0,  // Texture for wall type 1
+                2 => 4,  // Texture for wall type 2
+                3 => 11, // Texture for wall type 3
+                4 => 2,  // Texture for wall type 4
+                _ => 0,  // Default texture
             };
 
-            // Calculate the perpendicular distance to the wall
+            let mut texture = self.textures[texture_index].borrow_mut();
+            let tex_width = texture.width();
+            let tex_height = texture.height();
+
+            // Compute the perpendicular distance to the wall
             let dperp = if hit.1 == 0 { sidedist.x - deltadist.x } else { sidedist.y - deltadist.y };
-
-            // Calculate wall height (similar to floor calculation)
             let h = (self.screen_height as f32 / dperp) as i32;
-
-            // Calculate the y-coordinate range to draw the wall (similar to floor calculation)
             let y0 = ((self.screen_height / 2) - (h / 2)).max(0);
             let y1 = ((self.screen_height / 2) + (h / 2)).min(self.screen_height - 1);
 
-            // Texture mapping logic
+            // Compute texture X coordinate
             let hit_pos = pos + dir * dperp;
             let mut tex_x = if hit.1 == 0 {
                 hit_pos.y - hit_pos.y.floor()
@@ -165,24 +184,23 @@ impl Raycaster {
                 hit_pos.x - hit_pos.x.floor()
             };
 
-            // Adjust tex_x based on ray direction
+            // Flip texture coordinate based on ray direction
             if (hit.1 == 0 && dir.x > 0.0) || (hit.1 == 1 && dir.y < 0.0) {
                 tex_x = 1.0 - tex_x;
             }
 
-            let tex_x = (tex_x * texture.width() as f32) as i32;
-            let step = texture.height() as f32 / h as f32;
+            let tex_x = (tex_x * tex_width as f32) as i32;
+            let step = tex_height as f32 / h as f32;
             let mut tex_pos = (y0 as f32 - self.screen_height as f32 / 2.0 + h as f32 / 2.0) * step;
 
-            // Draw the wall pixels
             for y in y0..y1 {
-                let tex_y = (tex_pos as i32) & (texture.height() - 1);
+                let tex_y = (tex_height - 1 - (tex_pos as i32)) & (tex_height - 1);
                 tex_pos += step;
 
                 let color = texture.get_color(tex_x, tex_y);
 
-                // Draw the pixel at (x, y)
-                d.draw_pixel(x as i32, y as i32, color);
+                // Store the pixel in the buffer
+                self.pixelbuffer[(y * self.screen_width + x) as usize] = Self::color_to_u32(color);
             }
         }
     }
